647018d33939678b30ef17c0169133cfcc58cec7
Андрей <killumexxx@gmail.com>
2023-02-02 16:03:21 +0300

    node hiding the implementation

--- cypher/node.go
+++ cypher/node.go
@@ -6,63 +6,63 @@ import (
 )
 
 type Node struct {
-	*Label
-	Variable   string
-	Properties Props
+	variable   string
+	label      Label
+	properties Props
 }
 
 func NewNode() *Node {
 	return &Node{
-		Label:      NewLabel(),
-		Properties: Props{},
-		Variable:   "",
+		label:      Label{},
+		properties: Props{},
+		variable:   "",
 	}
 }
 
 func (n *Node) SetVariable(name string) *Node {
-	n.Variable = name
+	n.variable = name
 	return n
 }
 
 func (n *Node) SetProps(props ...Prop) *Node {
 	for _, p := range props {
-		n.Properties[p.Key] = p.Value
+		n.properties[p.Key] = p.Value
 	}
 	return n
 }
 
 func (n *Node) SetLabel(label string) *Node {
-	n.Names = append(n.Names, label)
+	n.label.Names = append(n.label.Names, label)
 	return n
 }
 
 func (n *Node) SetLabels(condition Condition, labels ...string) *Node {
-	n.Names = append(n.Names, labels...)
-	n.Condition = condition
+	n.label.Names = append(n.label.Names, labels...)
+	n.label.Condition = condition
 	return n
 }
 
-func (n *Node) ToPattern() QueryPattern {
-	return QueryPattern{OnlyNode: OnlyNode{Node: n}}
+func (n Node) ToPattern() QueryPattern {
+	return QueryPattern{OnlyNode: OnlyNode{Node: &n}}
 }
 
-func (n *Node) ToCypher() string {
+func (n Node) ToCypher() string {
 	node := "("
 
-	if n.Variable != "" {
-		node += n.Variable
+	if n.variable != "" {
+		node += n.variable
 	}
 
-	if len(n.Label.Names) > 0 {
+	if len(n.label.Names) > 0 {
 		condition := ""
-		if n.Label.Condition != "" {
-			condition = fmt.Sprintf("%v", n.Label.Condition)
+		if n.label.Condition != "" {
+			condition = fmt.Sprintf("%v", n.label.Condition)
 		}
-		node += fmt.Sprintf(":%v", strings.Join(n.Label.Names, condition)) + " "
+		node += fmt.Sprintf(":%v", strings.Join(n.label.Names, condition)) + " "
 	}
 
-	if len(n.Properties) > 0 {
-		node += n.Properties.ToCypher()
+	if len(n.properties) > 0 {
+		node += n.properties.ToCypher()
 	}
 
 	return node + ")"
