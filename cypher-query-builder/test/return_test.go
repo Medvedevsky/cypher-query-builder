e196815d5c804f38cdeac21e7e29059a0a341b36
Андрей <killumexxx@gmail.com>
2023-02-04 00:57:59 +0300

    feat: tests for return.go

---
+++ cypher-query-builder/test/return_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"test/neo4j/pkg/pattern"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReturnConfig_ToString(t *testing.T) {
+	req := require.New(t)
+	var err error
+	var cypher string
+
+	//name not defined
+	t1 := pattern.ReturnConfig{
+		Type: "a",
+		As:   "abc",
+	}
+	_, err = t1.ToString()
+	req.NotNil(err)
+
+	//pattern
+	t4 := pattern.ReturnConfig{
+		Name: "n",
+		Type: "m",
+	}
+	cypher, err = t4.ToString()
+	req.Nil(err)
+	req.EqualValues("n.m", cypher)
+
+	//pattern
+	t5 := pattern.ReturnConfig{
+		Name: "n",
+		Type: "m",
+		As:   "abc",
+	}
+	cypher, err = t5.ToString()
+	req.Nil(err)
+	req.EqualValues("n.m AS abc", cypher)
+}
