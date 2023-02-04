ab2851259c4b1e6395606ee2ec6df0f25beb9a28
Андрей <killumexxx@gmail.com>
2023-02-04 12:10:35 +0300

    feat: tests for prop.go

---
+++ cypher-query-builder/test/prop_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"test/neo4j/pkg/pattern"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestProp_ToCypher(t *testing.T) {
+	req := require.New(t)
+	var cypher string
+
+	//pattern
+	t1 := pattern.Props{}
+	t1["key"] = "value"
+	cypher = t1.ToCypher()
+	req.EqualValues("{key: 'value'}", cypher)
+
+	//pattern
+	t2 := pattern.Props{}
+	t2["key"] = true
+	cypher = t2.ToCypher()
+	req.EqualValues("{key: true}", cypher)
+
+	//pattern
+	t3 := pattern.Props{}
+	t3["key"] = 123
+	cypher = t3.ToCypher()
+	req.EqualValues("{key: 123}", cypher)
+}
